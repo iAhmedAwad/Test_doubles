446031ad5ac73271eddd4e4d4468439c6296ed33
Ahmed Awad <ahmed.mmawad@hotmail.com>
2021-09-07 15:51:04 +0200

    Updated testcase

--- app/src/main/java/com/trianglz/test_doubles/common/posts/domain/usecases/GetPostsUseCase.kt
+++ app/src/main/java/com/trianglz/test_doubles/common/posts/domain/usecases/GetPostsUseCase.kt
@@ -2,12 +2,16 @@ package com.trianglz.test_doubles.common.posts.domain.usecases
 
 import com.trianglz.test_doubles.common.posts.domain.models.PostDomainModel
 import com.trianglz.test_doubles.common.posts.domain.repository.PostsRepo
+import com.trianglz.test_doubles.common.posts.domain.repository.UselessRepo
 import javax.inject.Inject
 
-class GetPostsUseCase @Inject constructor(private val postsRepo: PostsRepo) {
+class GetPostsUseCase @Inject constructor(
+    private val postsRepo: PostsRepo,
+    private val uselessRepo: UselessRepo
+) {
 
-suspend fun execute(): List<PostDomainModel> {
-    return postsRepo.getPosts()
-}
+    suspend fun execute(): List<PostDomainModel> {
+        return postsRepo.getPosts().toSet().toList()
+    }
 
 }
