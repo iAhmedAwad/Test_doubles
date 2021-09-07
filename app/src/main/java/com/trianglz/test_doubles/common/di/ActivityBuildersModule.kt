c60bc85537e82c2095ff2db82438fcf518bb25f3
Ahmed Awad <ahmed.mmawad@hotmail.com>
2021-09-07 15:15:46 +0200

    Refactoring

--- app/src/main/java/com/trianglz/test_doubles/common/di/ActivityBuildersModule.kt
+++ app/src/main/java/com/trianglz/test_doubles/common/di/ActivityBuildersModule.kt
@@ -1,6 +1,6 @@
 package com.trianglz.test_doubles.common.di
 
-import com.trianglz.test_doubles.common.posts.di.PostsFragmentProvider
+import com.trianglz.test_doubles.modules.posts.di.PostsFragmentProvider
 import com.trianglz.test_doubles.MainActivity
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
