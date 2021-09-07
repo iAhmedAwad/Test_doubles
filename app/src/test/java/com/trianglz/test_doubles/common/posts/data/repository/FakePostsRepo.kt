845a3b7babc4f5b041c94aa9541a15ab26c96834
Ahmed Awad <ahmed.mmawad@hotmail.com>
2021-09-07 15:51:17 +0200

    Added FakePostsRepo

---
+++ app/src/test/java/com/trianglz/test_doubles/common/posts/data/repository/FakePostsRepo.kt
@@ -0,0 +1,29 @@
+package com.trianglz.test_doubles.common.posts.data.repository
+
+
+import com.trianglz.test_doubles.common.posts.domain.models.PostDomainModel
+import com.trianglz.test_doubles.common.posts.domain.repository.PostsRepo
+import kotlinx.coroutines.delay
+
+class FakePostsRepo : PostsRepo {
+    private val list = ArrayList<PostDomainModel>().apply {
+        repeat(10) {
+            val postDomainModel = PostDomainModel(
+                id = it, title = "Post number: $it",
+                body = "Body of post number: $it"
+            )
+            add(postDomainModel)
+        }
+        val postDomainModel = PostDomainModel(
+            id = 3, title = "Post number: 3",
+            body = "Body of post number: 3"
+        )
+        add(postDomainModel)
+    }
+
+    override suspend fun getPosts(): List<PostDomainModel> {
+        delay((2 * 60 * 1000).toLong())
+        return list
+    }
+
+}
