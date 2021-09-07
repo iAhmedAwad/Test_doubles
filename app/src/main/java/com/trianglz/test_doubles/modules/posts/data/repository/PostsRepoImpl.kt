8fe59c44c3b8aa76a7b33db09adcd1962d9d7149
Ahmed Awad <ahmed.mmawad@hotmail.com>
2021-09-07 18:22:14 +0200

    Implementing getPosts()

--- app/src/main/java/com/trianglz/test_doubles/modules/posts/data/repository/PostsRepoImpl.kt
+++ app/src/main/java/com/trianglz/test_doubles/modules/posts/data/repository/PostsRepoImpl.kt
@@ -1,5 +1,9 @@
 package com.trianglz.test_doubles.modules.posts.data.repository
 
+import android.util.Log
+import com.haroldadmin.cnradapter.NetworkResponse
+import com.haroldadmin.cnradapter.executeWithRetry
+import com.trianglz.test_doubles.modules.posts.data.mapper.toPostDomainModel
 import com.trianglz.test_doubles.modules.posts.data.service.PostsService
 import com.trianglz.test_doubles.modules.posts.domain.models.PostDomainModel
 import com.trianglz.test_doubles.modules.posts.domain.repository.PostsRepo
@@ -9,11 +13,35 @@ class PostsRepoImpl @Inject constructor(private val service: PostsService) : Pos
 
 
     override suspend fun addPost(post: PostDomainModel) {
-        throw AssertionError("Method not yet implemented")
+
     }
 
     override suspend fun getPosts(): List<PostDomainModel> {
-        throw Exception("Method is not implemented yet!")
+
+        val result = executeWithRetry(5){
+            service.getPosts()
+        }
+        when(result){
+            is NetworkResponse.Success -> {
+                return result.body.map { postDataResponseItem ->
+                    postDataResponseItem.toPostDomainModel() }
+            }
+            is NetworkResponse.NetworkError -> {
+                Log.d(TAG, "NetworkError: Error: ${result.error.message}")
+                return emptyList()
+            }
+
+            is NetworkResponse.ServerError -> {
+                Log.d(TAG, "ServerError: Error: ${result.body}")
+                return emptyList()
+
+            }
+            is NetworkResponse.UnknownError -> {
+                Log.d(TAG, "UnknownError: Error: ${result.error.message}")
+                return emptyList()
+
+            }
+        }
     }
 
     companion object {
--- app/src/main/java/com/trianglz/test_doubles/modules/posts/domain/usecases/GetPostsUseCase.kt
+++ app/src/main/java/com/trianglz/test_doubles/modules/posts/domain/usecases/GetPostsUseCase.kt
@@ -7,7 +7,8 @@ import javax.inject.Inject
 class GetPostsUseCase @Inject constructor(private val postsRepo: PostsRepo) {
 
     suspend fun addPost(postDomainModel: PostDomainModel) {
-        postsRepo.addPost(postDomainModel)
+     //   if (postDomainModel.id < 2)
+            postsRepo.addPost(postDomainModel)
     }
 
     suspend fun getPosts(): List<PostDomainModel> {
