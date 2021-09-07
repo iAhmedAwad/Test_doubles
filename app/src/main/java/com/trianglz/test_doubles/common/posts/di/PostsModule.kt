55bad53210ceb4ac2a2aa4e594d2a68110287996
Ahmed Awad <ahmed.mmawad@hotmail.com>
2021-09-07 15:51:28 +0200

    Binded UselessRepoImpl

--- app/src/main/java/com/trianglz/test_doubles/common/posts/di/PostsModule.kt
+++ app/src/main/java/com/trianglz/test_doubles/common/posts/di/PostsModule.kt
@@ -2,7 +2,9 @@ package com.trianglz.test_doubles.common.posts.di
 
 import com.trianglz.test_doubles.common.posts.data.service.PostsService
 import com.trianglz.test_doubles.common.posts.data.repository.PostsRepoImpl
+import com.trianglz.test_doubles.common.posts.data.repository.UselessRepoImpl
 import com.trianglz.test_doubles.common.posts.domain.repository.PostsRepo
+import com.trianglz.test_doubles.common.posts.domain.repository.UselessRepo
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
@@ -14,6 +16,10 @@ abstract class PostsModule {
     @PostsScope
     @Binds
     abstract fun bindPostsRepo(postsRepoImpl: PostsRepoImpl): PostsRepo
+
+    @PostsScope
+    @Binds
+    abstract fun bindUselessRepo(uselessRepoImpl: UselessRepoImpl): UselessRepo
 
     companion object {
         @PostsScope
