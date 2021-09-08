5c6c5a0cc505e9d928e3db58cc188aff7d1686e4
Ahmed Awad <ahmed.mmawad@hotmail.com>
2021-09-08 07:52:40 +0200

    Added getPosts_usingMockitoSizeIsFive_isTrue()

--- app/src/test/java/com/trianglz/test_doubles/modules/posts/domain/usecases/GetPostsUseCaseTestStub.kt
+++ app/src/test/java/com/trianglz/test_doubles/modules/posts/domain/usecases/GetPostsUseCaseTestStub.kt
@@ -2,8 +2,12 @@ package com.trianglz.test_doubles.modules.posts.domain.usecases
 
 import com.google.common.truth.Truth.assertThat
 import com.trianglz.test_doubles.modules.posts.data.repository.PostsRepoImplTestStub
+import com.trianglz.test_doubles.modules.posts.domain.models.PostDomainModel
+import com.trianglz.test_doubles.modules.posts.domain.repository.PostsRepo
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Test
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
 
 class GetPostsUseCaseTestStub {
 
@@ -13,8 +17,27 @@ class GetPostsUseCaseTestStub {
         runBlockingTest {
 
             val repo = PostsRepoImplTestStub()
-            val useCase=GetPostsUseCase(repo)
+            val useCase = GetPostsUseCase(repo)
 
+            val result = useCase.getFirstFivePosts()
+            assertThat(result.size).isEqualTo(5)
+        }
+    }
+
+    @Test
+    fun getPosts_usingMockitoSizeIsFive_isTrue() {
+
+        runBlockingTest {
+
+            val repo = mock(PostsRepo::class.java)
+            val useCase = GetPostsUseCase(repo)
+            val posts = ArrayList<PostDomainModel>().apply {
+                repeat(20) {
+                    val post = PostDomainModel(it, "title", "body")
+                    add(post)
+                }
+            }
+            `when`(repo.getPosts()).thenReturn(posts)
             val result = useCase.getFirstFivePosts()
             assertThat(result.size).isEqualTo(5)
         }
