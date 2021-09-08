77f333b77ce679988a0338e902dbe9f7f922270b
Ahmed Awad <ahmed.mmawad@hotmail.com>
2021-09-08 06:41:22 +0200

    Added comments

--- app/src/test/java/com/trianglz/test_doubles/common/posts/data/repository/FakePostsRepo.kt
+++ app/src/test/java/com/trianglz/test_doubles/common/posts/data/repository/FakePostsRepo.kt
@@ -5,9 +5,11 @@ import com.trianglz.test_doubles.modules.posts.domain.repository.PostsRepo
 import kotlinx.coroutines.delay
 
 class FakePostsRepo : PostsRepo {
+    //Like an in memory database
     private val list = ArrayList<PostDomainModel>()
 
     override suspend fun getPosts(): List<PostDomainModel> {
+        //Demonstrates the use of runBlockingTest
         delay((2 * 60 * 1000).toLong())
         return list
     }
